83deeb77594243205d5a02e2e55c6084aa26cd6c
Giorgos Tzampanakis <giorgos.tzampanakis@gmail.com>
2017-03-19 23:29:07 +0200

    64: WIP

--- 64.py
+++ 64.py
@@ -1,4 +1,6 @@
 from itertools import *
+import pmemoize
+import random
 
 class S:
 	def __init__(self, a):
@@ -6,22 +8,46 @@ class S:
 	def __str__(self):
 		return 'sqrt(%s)' % self.a
 
-def xyz(a,b,c,m):
-	assert (a.a - c**2) % b == 0
+@pmemoize.MemoizedFunction
+def yz(a,b,c,m):
+	# assert (a.a - c**2) % b == 0
 	newd = (a.a - c**2)/b
-	return m*newd/newd, c-m*newd, newd
+	return c-m*newd, newd
 
+@pmemoize.MemoizedFunction
 def inf(a):
 	a0 = int(a.a**.5)
 	b = 1
 	c = a0
-	ms = cycle([1,3,1,8])
+	ms = cycle([1,1,1,4])
 	for _ in xrange(30):
 		m = ms.next()
-		x,y,z = xyz(a,b,c,m)
-		print x,y,z
+		y,z = yz(a,b,c,m)
+		print m,y,z
 		a=a
 		b=z
 		c=-y
+
+def find(a):
+	for ms in product(range(1, 9), repeat=6):
+		ms = list(ms)
+		ms_used = []
+		yzs = []
+		a0 = int(a.a**.5)
+		b = 1
+		c = a0
+		while ms:
+			m = ms.pop()
+			y,z = yz(a,b,c,m)
+			if yzs and (y,z) == yzs[0]:
+				return len(ms_used), ms_used
+			yzs.append((y,z))
+			ms_used.append(m)
+			a=a
+			b=z
+			c=-y
 	
-inf(S(23))
+for n in xrange(2, 50):
+	if n**.5 == int(n**.5):
+		continue
+	print n, find(S(n))
